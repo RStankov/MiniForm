8f755701ea5d89d727a87e74f956dfa8f47537b7
Radoslav Stankov <rstankov@gmail.com>
2015-02-18 17:12:27 +0200

    Add validations for nested validation

---
+++ spec/formi/nested_validator_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+module Formi
+  describe NestedValidator do
+    class User
+      include ActiveModel::Model
+
+      attr_accessor :name
+
+      validates :name, presence: true
+    end
+
+    class Record < Struct.new(:user)
+      include ActiveModel::Validations
+    end
+
+    let(:validator) { NestedValidator.new(attributes: [:user]) }
+    let(:user)      { User.new }
+    let(:record)    { Record.new(user) }
+
+    it 'copies errors from submodel to model' do
+      validator.validate(record)
+
+      expect(record.errors[:name]).not_to be_blank
+    end
+
+    it 'does not copy errors when there are not any' do
+      user.name = 'valid name'
+
+      validator.validate(record)
+
+      expect(record.errors[:name]).to be_blank
+    end
+  end
+end
